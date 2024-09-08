0cf2ac794d53de8dfd78bd940d42b4ab3e30ca4a
Jagad Brahma Wiraatmaja <officialelsa21@gmail.com>
2024-09-08 16:29:43 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 import createMDX from '@next/mdx'
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
@@ -50,6 +50,7 @@ const nextConfig = {
   output: 'standalone',
 }
 
+// Mengonfigurasi MDX dengan ESM
 const withMDX = createMDX({
   extension: /\.mdx?$/,
 })
