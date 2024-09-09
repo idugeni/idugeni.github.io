8a8991f4270758b572e9a37b9e9c801dd83cc34b
Jagad Brahma Wiraatmaja <officialelsa21@gmail.com>
2024-09-09 08:19:25 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,30 @@
-import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.jsdelivr.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'nextjs.org', pathname: '/**' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org', pathname: '/**' },
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nextjs.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
     ],
   },
   reactStrictMode: true,
@@ -16,28 +32,26 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
-  transpilePackages: ['next-mdx-remote'],
   experimental: {
     turbo: {
-      enabled: true,
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
     },
-    serverActions: {
-      enabled: true,
-    },
-    mdxRs: {
-      jsxRuntime: 'automatic',
-      jsxImportSource: 'react',
-      mdxType: 'gfm',
-    },
+    after: true,
   },
+  transpilePackages: ['next-mdx-remote'],
+  output: 'standalone',
 }
 
 const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
+  extension: /\.mdx?$/,
 })
 
 export default withMDX(nextConfig)
